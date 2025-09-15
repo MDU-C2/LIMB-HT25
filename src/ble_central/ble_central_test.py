3db32938a650fb6e6b40fdad3a68b6e5468a40fc
Johan Sandred <jsd21003@student.mdu.se>
2025-09-15 10:38:49 +0200

    Add a python BLE Central test program

---
+++ src/ble_central/ble_central_test.py
@@ -0,0 +1,208 @@
+"""Bluetooth Low Energy Central test program."""
+
+from __future__ import annotations
+
+import asyncio
+from typing import TYPE_CHECKING, Callable
+
+from bleak import (
+    BleakClient,
+    BleakScanner,
+)
+
+if TYPE_CHECKING:
+    from bleak.backends.characteristic import BleakGATTCharacteristic
+
+SERVICE_UUID = "23011525-1212-efde-1523-785feabcd122"
+EMG_CHARACTERISTIC_UUID = "24011525-1212-efde-1523-785feabcd122"
+IMU_CHARACTERISTIC_UUID = "25011525-1212-efde-1523-785feabcd122"
+PIEZO_CHARACTERISTIC_UUID = "26011525-1212-efde-1523-785feabcd122"
+
+
+def decode_seq_num(arr: bytearray) -> int:
+    """Extract the sequence number from the packet data."""
+    return int.from_bytes(arr[:4], "little")
+
+
+def print_received_packets_stats(packets: list[bytearray], sensor: str) -> None:
+    """Print details about the packets received."""
+    print(f"{sensor}")
+    notification_count = len(packets)
+    print(f"notification count: {notification_count}")
+    seq_nrs = [decode_seq_num(x) for x in packets]
+    print(
+        f"seq nrs: {seq_nrs}",
+    )
+    starting_sequence_number = decode_seq_num(packets[0])
+    i = starting_sequence_number
+    for arr in packets:
+        seqnr = decode_seq_num(arr)
+        if seqnr > i:
+            print(f"{i} to {seqnr - 1} ({seqnr - i} packets) are missing.")
+            i = seqnr
+        i += 1
+    print(f"last seq nr received: {i - 1}")
+    seq_nr_range = i - starting_sequence_number
+    print(f"total range of seq nr: {seq_nr_range}")
+    missing = seq_nr_range - notification_count
+
+    print(f"missed packets: {missing} ({missing / i * 100:.2f}%)")
+
+
+def set_up_notify_handler(
+    emg_buf: list[bytearray | None],
+    imu_buf: list[bytearray | None],
+    piezo_buf: list[bytearray | None],
+) -> tuple[
+    Callable[[BleakGATTCharacteristic, bytearray], None],
+    Callable[[BleakGATTCharacteristic, bytearray], None],
+    Callable[[BleakGATTCharacteristic, bytearray], None],
+]:
+    """Return the EMG, IMU, and piezo characteristic handlers."""
+    first_emg_sequence_number = 0
+    first_imu_sequence_number = 0
+    first_piezo_sequence_number = 0
+
+    def emg_notify_handler(
+        characteristic: BleakGATTCharacteristic,
+        data: bytearray,
+    ) -> None:
+        # print(f"Read EMG [{characteristic.uuid}]")
+        i = decode_seq_num(data)
+        nonlocal first_emg_sequence_number
+        if first_emg_sequence_number == 0:
+            first_emg_sequence_number = i
+        emg_buf[i - first_emg_sequence_number] = data
+
+    def imu_notify_handler(
+        characteristic: BleakGATTCharacteristic,
+        data: bytearray,
+    ) -> None:
+        # print(f"Read IMU [{characteristic.uuid}]")
+        i = decode_seq_num(data)
+        nonlocal first_imu_sequence_number
+        if first_imu_sequence_number == 0:
+            first_imu_sequence_number = i
+        imu_buf[i - first_imu_sequence_number] = data
+
+    def piezo_notify_handler(
+        characteristic: BleakGATTCharacteristic,
+        data: bytearray,
+    ) -> None:
+        # print(f"Read piezo [{characteristic.uuid}]")
+        i = decode_seq_num(data)
+        nonlocal first_piezo_sequence_number
+        if first_piezo_sequence_number == 0:
+            first_piezo_sequence_number = i
+        piezo_buf[i - first_piezo_sequence_number] = data
+
+    return (emg_notify_handler, imu_notify_handler, piezo_notify_handler)
+
+
+async def main() -> None:
+    """Subscribe to sensor data from peripheral and count dropped packets."""
+    timeout = 10
+    print(f"scanning for {timeout} seconds, please wait...")
+
+    device = await BleakScanner.find_device_by_name(
+        "LIMBServer",
+        timeout,
+    )
+    if not device:
+        print("Couldn't find device.")
+        return
+
+    print(f"Found device {device}!")
+    print(f"Details: {device.details}")
+
+    def disconnect_handler(bc: BleakClient) -> None:
+        print(f"Disconnected from {bc.address}.")
+
+    emg_data_received: list[bytearray | None] = [None] * 10000
+    imu_data_received: list[bytearray | None] = [None] * 10000
+    piezo_data_received: list[bytearray | None] = [None] * 10000
+
+    (emg_notify_handler, imu_notify_handler, piezo_notify_handler) = (
+        set_up_notify_handler(emg_data_received, imu_data_received, piezo_data_received)
+    )
+
+    async with BleakClient(device, disconnect_handler) as client:
+        print(f"Connected: {client.is_connected}")
+        print(f"Service handles: {client.services.services.keys()}")
+
+        service = client.services.get_service(SERVICE_UUID)
+        if service is None:
+            print("Error: Couldn't get services.")
+            return
+        print(f"Sensor service: {service}")
+        print(f"Service UUID is valid: {service.uuid == SERVICE_UUID}")
+        print(f"Characteristic count: {len(service.characteristics)}")
+
+        emg_chr = service.get_characteristic(EMG_CHARACTERISTIC_UUID)
+        if emg_chr is None:
+            print("Error: Couldn't get EMG characteristic.")
+            return
+        imu_chr = service.get_characteristic(IMU_CHARACTERISTIC_UUID)
+        if imu_chr is None:
+            print("Error: Couldn't get IMU characteristic.")
+            return
+        piezo_chr = service.get_characteristic(PIEZO_CHARACTERISTIC_UUID)
+        if piezo_chr is None:
+            print("Error: Couldn't get piezo characteristic.")
+            return
+
+        # Single read from characteristic.
+        print()
+        emg_value_fut = client.read_gatt_char(emg_chr)
+        imu_value_fut = client.read_gatt_char(imu_chr)
+        piezo_value_fut = client.read_gatt_char(piezo_chr)
+        emg_value = await emg_value_fut
+        imu_value = await imu_value_fut
+        piezo_value = await piezo_value_fut
+
+        print(f"EMG char (including sequence number): {emg_value}")
+        print()
+        print(f"IMU char (including sequence number): {imu_value}")
+        print()
+        print(f"Piezo char (including sequence number): {piezo_value}")
+        print()
+
+        subscribe_period = 1
+        print(f"Starting subscription for {subscribe_period} seconds...")
+
+        # Subscribe to characteristics.
+        emg_fut = client.start_notify(emg_chr, emg_notify_handler)
+        imu_fut = client.start_notify(imu_chr, imu_notify_handler)
+        piezo_fut = client.start_notify(piezo_chr, piezo_notify_handler)
+        await emg_fut
+        await imu_fut
+        await piezo_fut
+
+        # Sleeping for 1 second means the expected amount of packets received is
+        # frequency / sample count per packet.
+        await asyncio.sleep(subscribe_period)
+
+        emg_fut = client.stop_notify(emg_chr)
+        imu_fut = client.stop_notify(imu_chr)
+        piezo_fut = client.stop_notify(piezo_chr)
+        await emg_fut
+        await imu_fut
+        await piezo_fut
+
+        print("Stopped subscription!")
+
+        filtered_emg_data_received = [x for x in emg_data_received if x is not None]
+        filtered_imu_data_received = [x for x in imu_data_received if x is not None]
+        filtered_piezo_data_received = [x for x in piezo_data_received if x is not None]
+
+        print()
+        print_received_packets_stats(filtered_emg_data_received, "EMG")
+        print()
+        print_received_packets_stats(filtered_imu_data_received, "IMU")
+        print()
+        print_received_packets_stats(filtered_piezo_data_received, "Piezo")
+        print()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
