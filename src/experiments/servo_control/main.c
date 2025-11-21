ea7a34c12e43f8cafee07097e323d248aa97b4fe
bugmobil <you@example.com>
2025-11-21 15:16:52 +0100

    Reorganize servo testing sequence for improved readability

--- src/experiments/servo_control/main.c
+++ src/experiments/servo_control/main.c
@@ -14,15 +14,16 @@ void app_main()
     
     ESP_LOGI(TAG, "Testing individual servos one by one...");
     
-    servo_write_deg_channel(0, 0);      // Thumb to min
-    servo_write_deg_channel(1, 0);      // Index to min
-    servo_write_deg_channel(2, 0);      // Middle to min
-    servo_write_deg_channel(3, 180);      // Ring to min
-    servo_write_deg_channel(4, 180);      // Pinky to min
-    vTaskDelay(pdMS_TO_TICKS(2000));
+   
     while(1) {
     
-    
+        servo_write_deg_channel(0, 0);      // Thumb to min
+        servo_write_deg_channel(1, 0);      // Index to min
+        servo_write_deg_channel(2, 0);      // Middle to min
+        servo_write_deg_channel(3, 180);      // Ring to min
+        servo_write_deg_channel(4, 180);      // Pinky to min
+        vTaskDelay(pdMS_TO_TICKS(2000));
+        
         servo_write_deg_channel(0, 180);      // Thumb to min
         servo_write_deg_channel(1, 180);      // Index to min
         servo_write_deg_channel(2, 180);      // Middle to min
