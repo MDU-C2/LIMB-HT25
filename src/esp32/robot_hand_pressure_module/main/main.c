4f9778162a8b4be2a2c9836aeb901be5c705930e
Johan Sandred <jsd21003@student.mdu.se>
2026-04-14 10:58:13 +0200

    Fix passing wrong number of arguments to can_send

--- src/esp32/robot_hand_pressure_module/main/main.c
+++ src/esp32/robot_hand_pressure_module/main/main.c
@@ -82,7 +82,7 @@ void loop_control(void) {
                 servo_angle = 180;
                 uint8_t open_msg = (uint8_t)servo_angle;
                 // NOTE: We're using the thumb activation as a hack. Sorry.
-                can_send(CAN_ID_ROBOT_THUMB_ACTUATION, &open_msg, 1);
+                can_send(CAN_ID_ROBOT_THUMB_ACTUATION, &open_msg, 1, 0);
             }
         }    
         
@@ -202,7 +202,7 @@ void loop_control(void) {
             // Static variable to ensure the CAN message only sends ONCE
             servo_angle = 10;
             can_msg_data = (uint8_t)servo_angle;
-            can_send(CAN_ID_ROBOT_THUMB_ACTUATION, &can_msg_data, 1);
+            can_send(CAN_ID_ROBOT_THUMB_ACTUATION, &can_msg_data, 1, 0);
 
             vTaskDelay(pdMS_TO_TICKS(5000));
             break;
