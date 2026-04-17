e35cbb77dd964aea114c0d1292968cde900995b7
Johan Sandred <jsd21003@student.mdu.se>
2026-04-17 14:36:49 +0200

    Modify the elbow's range of motion

--- src/esp32/robot_elbow_module/main/main.c
+++ src/esp32/robot_elbow_module/main/main.c
@@ -71,7 +71,7 @@ static const stepper_control_config_t s_elbow_stepper_cfg = {
             // Using the ratio between the joint angles and potentiomter angles,
             // 72 degrees of potentiometer angle becomes 60 degrees
             // of joint angle.
-            .max_potentiometer_angle = {170.F},
+            .max_potentiometer_angle = {165.F},
             .joint_angle_to_potentiometer_angle_ratio = 18.F / 15.F,
         },
 };
