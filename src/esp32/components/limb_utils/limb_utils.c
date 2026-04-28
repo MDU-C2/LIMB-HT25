5a914b92cb749b091fb6ea91dac4d17f72242a53
Johan Sandred <jsd21003@student.mdu.se>
2026-04-28 14:32:17 +0200

    Utils: Add float marshalling functionality
    When sending data over the wire, we want to send it in little-endian
    byte order. For integers this can be done with the htole** and le**toh
    macros, but there's no such macro for floats, so this commit adds that
    functionality. Additionally, a function is added to deserialize a float
    that has been received as a byte buffer. The deserialization is
    performed in a way that avoids strict aliasing issues.

--- src/esp32/components/limb_utils/limb_utils.c
+++ src/esp32/components/limb_utils/limb_utils.c
@@ -1,7 +1,35 @@
 #include "limb_utils.h"
 
+#include <endian.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
+#include "include/limb_utils.h"
+
+float deserialize_float(uint8_t *float_buf, FromEndian from_endian) {
+  // To extract the float from the byte buffer while avoiding strict aliasing
+  // issues, we use memcpy. This assumes that float_buf is at least 4 bytes
+  // long.
+  float ret = 0;
+  memcpy(&ret, float_buf, sizeof(ret));
+  if (from_endian == kFromBigEndian) {
+    return beftoh(ret);
+  }
+  return leftoh(ret);
+}
+
+float bswapf(float float_val) {
+  // To perform the byte swap in a standard-compliant way, we memcpy the bytes
+  // to an integer before calling bswap32, then memcpy the bytes back to the
+  // float variable.
+  uint32_t int_representation = 0;
+  static_assert(sizeof(float_val) == sizeof(int_representation),
+                "float is not 32 bits");
+  memcpy(&int_representation, &float_val, sizeof(int_representation));
+  int_representation = bswap32(int_representation);
+  memcpy(&float_val, &int_representation, sizeof(float_val));
+  return float_val;
+}
 
 uint16_t limb_average16(const uint16_t *values, size_t n) {
   uint32_t sum = 0;
